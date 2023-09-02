6ffbb0a6c88f775ce2a3b287849310cea4deca41
IamAnkitR <ardb8755@gmail.com>
2023-09-02 22:53:01 +0530

    updated matches component

--- sport-bee/src/pages/matches/MatchList.tsx
+++ sport-bee/src/pages/matches/MatchList.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useReducer, useEffect } from "react";
 import { API_ENDPOINT } from "../../config/constants";
-// import { Link } from "react-router-dom";
+import "./MatchList.css";
 
 interface Match {
   id: number;
   name: string;
   sportName: string;
   isRunning: boolean;
+  location: string;
   summary: string;
   endsAt: string;
   teams: [];
@@ -67,6 +68,7 @@ const MatchList: React.FC = () => {
         },
       });
       const data = await response.json();
+      console.log(data);
       dispatch({ type: "API_CALL_END", payload: data.matches });
     } catch (error) {
       console.log("Error fetching articles:", error);
@@ -75,24 +77,33 @@ const MatchList: React.FC = () => {
   };
   return (
     <>
-      <div className="flex pt-4 border-b-2 border-gray-400 pb-6 bg-gray-300 pl-6">
-        {state.matches.map(
-          (match) =>
-            match.isRunning && (
-              <div
-                key={match.id}
-                className="m-2 p-2 pt-4 pb-4 w-80 border-2 border-black rounded-lg"
-              >
-                <div>
-                  <h2 className="text-lg font-semibold">{match.sportName}</h2>
+      <div className="flex gap-4 w-full border-b-2 border-gray-400 pt-6 pb-6 bg-gray-300  overflow-x-scroll">
+        {state.matches.map((match) => (
+          <div
+            key={match.id}
+            className="ml-2 flex-shrink-0 h-36 w-64 p-2 border-2 border-black rounded-lg"
+          >
+            <div>
+              <h2 className="text-lg font-semibold">{match.sportName}</h2>
+            </div>
+            <div key={match.endsAt}>
+              <h1 className="font-bold">{match.name.split("at")[0]}</h1>
+              <div className="flex mt-2 ">
+                <div className="pt-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="1em"
+                    viewBox="0 0 384 512"
+                  >
+                    <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
+                  </svg>
                 </div>
-                <div key={match.endsAt}>
-                  <h1 className="font-bold">{match.name.split("at")[0]}</h1>
-                  <h1>{match.endsAt.slice(0, 10)}</h1>
-                </div>
+
+                <h1 className="pl-2">{match.location}</h1>
               </div>
-            )
-        )}
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
